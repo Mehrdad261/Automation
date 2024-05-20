8379a81b95dab3b4c519f605c46544549a8e2963
Mehrdad261 <61563408+Mehrdad261@users.noreply.github.com>
2024-05-20 16:54:14 +0100

    Update Users_status.ps1

--- Users_status.ps1
+++ Users_status.ps1
@@ -1,41 +1,46 @@
 # Before use this code you hould install Excel-module by : " Install-Module -Name ImportExcel -Scope CurrentUser -Force "
 try {
+    #Active Directory module
     Import-Module ActiveDirectory -ErrorAction Stop
+    
+    #ImportExcel module
     Import-Module ImportExcel -ErrorAction Stop
 
-    # Set the inactivityDays for inactivity in days
-    $inactivityDays = 30
+    #Set the Period for inactivity in days
+    $PeriodDays = 30
 
-    # Get the current date and calculate the inactivity date
+    #Get the current date and calculate the Period date
     $currentDate = Get-Date
-    $thresholdDate = $currentDate.AddDays(-$inactivityDays)
+    $PeriodDate = $currentDate.AddDays(-$PeriodDays)
 
-    # Initialize an empty array to hold the report data
+    #empty array to hold the report data
     $report = @()
 
-    # Get all users from Active Directory, filtering by objectClass to ensure only user objects are retrieved
-    $users = Get-ADUser -Filter { objectClass -eq "user" } -Property DisplayName, LastLogonDate, Enabled -ErrorAction Stop
+    #Get all users from Active Directory, filtering by objectClass to ensure only user objects are retrieved
+    $users = Get-ADUser -Filter { objectClass -eq "user" } -Property DisplayName, SamAccountName, LastLogonDate, Enabled -ErrorAction Stop
 
     foreach ($user in $users) {
         try {
-            # Determine user status
+            #Determine user status
             $status = if ($user.Enabled) { "Active" } else { "Disabled" }
 
-            # Check if LastLogonDate property is available and not null
+            #Check if LastLogonDate property is available and not null
             if ($null -ne $user.LastLogonDate) {
-                # Compare the LastLogonDate with the threshold date
-                if ($user.LastLogonDate -lt $thresholdDate) {
-                    # If the user has not logged in within the threshold, add them to the report
+                #Compare the LastLogonDate with the Period date
+                if ($user.LastLogonDate -lt $PeriodDate) {
+                    #If the user has not logged in within the Period, add them to the report
                     $report += [PSCustomObject]@{
-                        DisplayName   = $user.DisplayName
-                        LastLogonDate = $user.LastLogonDate
-                        Status        = $status
+                        DisplayName   = $user.DisplayName #Full Name 
+                        Username      = $user.SamAccountName #Username
+                        LastLogonDate = $user.LastLogonDate #last logindate 
+                        Status        = $status #USer's status (Active, disabled)
                     }
                 }
             } else {
-                # If LastLogonDate is null, consider the user as never logged in
+                #If LastLogonDate is null, consider the user as never logged in
                 $report += [PSCustomObject]@{
                     DisplayName   = $user.DisplayName
+                    SamAccountName = $user.SamAccountName  #Include Username 
                     LastLogonDate = "Never Logged In"
                     Status        = $status
                 }
@@ -45,18 +50,27 @@ try {
         }
     }
 
-    # Output the report to console
-    if ($report.Count -gt 0) {
-        $report | Format-Table -AutoSize
-    } else {
-        Write-Output "All users have logged in within the last $inactivityDays days."
+    # ----------------- Console output report -----------------------
+    #if ($report.Count -gt 0) {
+    #    $sortedReport = $report | Sort-Object DisplayName 
+    #    $sortedReport | Format-Table -AutoSize
+    #} else {
+    #    Write-Output "All users have logged in within the last $PeriodDays days."
+    #}
+    #-------------------------------------------------------------------------------
+
+    # folder Path for report
+    $folderPath= "C:\Users_Report"
+    $excelPath = "$folderPath\report.xls"
+
+    # check for existing folder 
+    if (-not (Test-Path -Path $folderPath)){
+        New-Item -ItemType Directory -Path $folderPath
     }
+     # Export the report to an Excel file
+    $sortedReport | Export-Excel -Path $excelPath -WorksheetName "User Report" -AutoSize
 
-    # Export the report to an Excel file
-    $excelPath = "D:\Automation\Report\report.xls"
-    $report | Export-Excel -Path $excelPath -WorksheetName "User Report" -AutoSize
-
-    Write-Output "Report successfully exported to $excelPath"
+    Write-Output "Report successfully exported to $excelPath" 
 
 } catch {
     Write-Error "An error occurred: $_"
